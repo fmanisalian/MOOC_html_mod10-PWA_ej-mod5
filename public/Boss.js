cb9a76504e17e75789edd897a259e6a2c94b4d2b
fmanisalian <fermancba@gmail.com>
2020-11-16 20:59:42 -0300

    shooterPWA

---
+++ public/Boss.js
@@ -0,0 +1,76 @@
+/**
+ * Jefe-Monstruo al que tenemos que destruir
+ */
+class Boss extends Character {
+    /**
+     * @param game {Game} La instancia del juego al que pertenece el oponente
+     */
+    constructor (game) {
+        const height = OPPONENT_HEIGHT * game.width / 100,
+            width = OPPONENT_WIDTH * game.width / 100,
+            x = getRandomNumber(game.width - width / 2),
+            y = 0,
+            speed = JEFE_SPEED,
+            myImage = JEFE_PICTURE,
+            myImageDead = JEFE_PICTURE_DEAD;      
+
+        super(game, width, height, x, y, speed, myImage, myImageDead);
+        this.direction = "R";        
+        setTimeout(() => this.shoot(), 1000 + getRandomNumber(2500));
+    }
+
+
+        /**
+     * Crea un nuevo disparo
+     */
+    shoot () {
+        if (!this.dead && !this.game.ended) {
+            if (!this.game.paused) {
+                this.game.shoot(this);
+            }
+            setTimeout(() => this.shoot(), 1000 + getRandomNumber(2500));
+        }
+    }
+
+    /**
+     * Actualiza los atributos de posición del oponente
+     */
+    update () {
+        if (!this.dead && !this.game.ended) {
+            this.y += this.speed;
+            if (this.y > this.game.height) {
+                this.y = 0;
+            }
+            if (this.direction === "R") { // Hacia la derecha
+                if (this.x < this.game.width - this.width - this.speed) {
+                    this.x += this.speed;
+                } else {
+                    this.horizontalMov = 0;
+                }
+            } else if (this.x > this.speed) {
+                this.x -= this.speed;
+            } else {
+                this.horizontalMov = 0;
+            }
+            this.horizontalMov -= this.speed;
+            if (this.horizontalMov < this.speed) {
+                this.horizontalMov = getRandomNumber(this.game.width / 2);
+                this.direction = this.direction === "R" ? "L" : "R"; // Cambia de sentido
+            }
+        }
+    }
+
+   
+    /**
+     * Mata al oponente
+     */
+    collide() { 
+
+        if (!this.dead) {
+            this.game.score += 1;
+            document.getElementById("scoreli").innerHTML = "Score: " + this.game.score; 
+            super.collide();
+            this.game.endGame();
+        }
+    }
+}
